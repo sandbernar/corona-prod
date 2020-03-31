401c9de0abd30f6959ffb327b41f56faf9c72fb1
Tlekbai Ali <tlekbalkai@gmail.com>
2020-03-31 21:29:11 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -56,7 +56,7 @@ sudo service nginx reload
 
 # install docker
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSLk https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 sudo apt install docker-ce
@@ -64,7 +64,7 @@ sudo usermod -aG docker ${USER}
 su - ${USER}
 
 # docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -Lk https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # install postgres
