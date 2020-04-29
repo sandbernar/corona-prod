1c70a44da90f7b41a662625d48d42ced8108ef22
Artem Fedoskin <afedoskin3@gmail.com>
2020-04-29 13:22:05 +0600

    Give delete option to Nur-Sultan and Almaty

--- web-app/app/main/patients/routes.py
+++ web-app/app/main/patients/routes.py
@@ -514,7 +514,11 @@ def patient_profile():
 
             states = PatientState.query.filter_by(patient_id=patient.id).join(State).all()
 
-            return route_template('patients/profile', states=states, patient=patient, age=age, hospital_name=hospital_name, form = form, change = change, c=c, travel=travel)
+            regions_with_delete = [Region.query.filter_by(name="Алматы").first().id,
+                                    Region.query.filter_by(name="Нур-Султан ").first().id]
+
+            return route_template('patients/profile', states=states, patient=patient, age=age, hospital_name=hospital_name,
+                                    form = form, change = change, c=c, travel=travel, regions_with_delete=regions_with_delete)
     else:    
         return render_template('errors/error-500.html'), 500
 
