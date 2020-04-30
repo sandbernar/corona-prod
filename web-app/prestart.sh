2ff3d2f201fe2b00398e7f91eb9b3a7de53f38e4
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2020-04-30 17:39:41 +0600

    fix: prestart

--- web-app/prestart.sh
+++ web-app/prestart.sh
@@ -1,7 +1,3 @@
 
 python initLoggingTrigger.py
-<<<<<<< HEAD
-#python manage.py db upgrade
-=======
-# python manage.py db upgrade
->>>>>>> 2f6090023a10bc276edce59e5df7e6d2e788ab67
+python manage.py db upgrade
