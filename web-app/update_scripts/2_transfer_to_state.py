1929e0c080b6754679e479807f3c2ce4df5f4ce9
Ali Tlekbai <ali.tlekbai@gmail.com>
2020-05-06 12:37:55 +0600

    testing

--- web-app/update_scripts/2_transfer_to_state.py
+++ web-app/update_scripts/2_transfer_to_state.py
@@ -6,6 +6,8 @@ from datetime import datetime, timedelta
 
 import psycopg2
 
+from app import constants as C
+
 # Connect to our PostgreSQL
 psqlConn = psycopg2.connect(dbname=os.getenv("DATABASE_NAME"),
                             user=os.getenv("DATABASE_USER"),
@@ -59,7 +61,7 @@ def getDetectionDate(patient_id, status_name):
         ))
         if result is None or len(result) == 0:
             return datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S")
-        return "T".join(result[0]["tstamp"].split())
+        return datetime.strftime(result[0]["tstamp"], "%Y-%m-%dT%H:%M:%S")
     elif status_name == "Инфицирован":
         result = psqlQuery("""
             SELECT * FROM logging.t_history 
@@ -70,18 +72,19 @@ def getDetectionDate(patient_id, status_name):
         ))
         if result is None or len(result) == 0:
             return datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S")
-        return "T".join(result[0]["tstamp"].split())
+        return datetime.strftime(result[0]["tstamp"], "%Y-%m-%dT%H:%M:%S")
     result = psqlQuery("""
         SELECT * FROM logging.t_history 
         WHERE tabname='Patient' AND new_val->>'id'='%d' AND 
                 new_val->>'status_id'='%d' AND (old_val IS NULL OR old_val->>'status_id'!='%d')
         ;""" % (
         patient_id,
+        status[status_name],
         status[status_name]
     ))
     if result is None or len(result) == 0:
         return datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S")
-    return "T".join(result[0]["tstamp"].split())
+    return datetime.strftime(result[0]["tstamp"], "%Y-%m-%dT%H:%M:%S")
 
 """
     Transfer is_found, is_infected, status_id (PatientStatus) to attrs
@@ -92,26 +95,27 @@ for patient in patients:
     statuses = []
     if patient["attrs"].get("is_found", False):
         statuses.append("Найден")
-    if patient["attrs"].get("is_infected", False):
+    if patient["attrs"].get("status", None):
         if "Найден" not in statuses:
             statuses.append("Найден")
-        statuses.append("Инфицирован")
-    if patient["attrs"].get("status", None):
         statuses.append(patient["attrs"].get("status"))
+    if patient["attrs"].get("is_infected", False):
+        statuses.append("Инфицирован")
+    
 
     patientStates = psqlQuery('SELECT * FROM "PatientState" WHERE patient_id=%d' % patient["id"])
-    for status in statuses:
+    for st in statuses:
         found = False
         for patientState in patientStates:
-            if states.get(patientState["id"]) == status:
+            if states.get(patientState["state_id"]) == st:
                 found = True
                 break
         if not found:
             now = datetime.now()
             now = datetime.strftime(now, "%Y-%m-%dT%H:%M:%S")
-            detection_date = getDetectionDate(patient["id"], status)
+            detection_date = getDetectionDate(patient["id"], st)
             psqlQuery('INSERT INTO "PatientState" (state_id, patient_id, created_at, detection_date, attrs) VALUES (%d, %d, \'%s\',\'%s\', \'{}\');' % (
-                states.get(status), patient["id"], now, detection_date
+                states.get(st), patient["id"], now, detection_date
             ))
 
 
