2b3fc38108f3552009436b16587249ff2cff708b
Tlekbai Ali <tlekbalkai@gmail.com>
2020-05-09 10:26:54 +0600

    add print 2_transfer

--- web-app/update_scripts/2_transfer_to_state.py
+++ web-app/update_scripts/2_transfer_to_state.py
@@ -123,6 +123,7 @@ for patient in patients:
         WHERE tabname='Patient' AND new_val->>'id'='%d' AND 
                 new_val->>'is_infected'='false' AND old_val->>'is_infected'='true' ORDER BY tstamp
         ;""" % (patient["id"]))
+    print("recovery", result)
     if result is not None or len(result) > 0:
         now = datetime.now()
         now = datetime.strftime(now, "%Y-%m-%dT%H:%M:%S")
