662bf0c477d97b21e78385801a2cb3c0bdc0ec6e
Artem Fedoskin <afedoskin3@gmail.com>
2020-06-13 20:20:04 +0600

    Disable Celery in main patients

--- web-app/app/main/patients/routes.py
+++ web-app/app/main/patients/routes.py
@@ -163,25 +163,25 @@ def prepare_patient_form(patient_form, with_old_data = False, with_all_travel_ty
 
     return patient_form
 
-@celery.task(bind=True)
-def long_task(self):
-    """Background task that runs a long function with progress reports."""
-    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
-    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
-    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
-    message = ''
-    total = random.randint(10, 50)
-    for i in range(total):
-        if not message or random.random() < 0.25:
-            message = '{0} {1} {2}...'.format(random.choice(verb),
-                                              random.choice(adjective),
-                                              random.choice(noun))
-        self.update_state(state='PROGRESS',
-                          meta={'current': i, 'total': total,
-                                'status': message})
-        time.sleep(1)
-    return {'current': 100, 'total': 100, 'status': 'Task completed!',
-            'result': 42}
+# @celery.task(bind=True)
+# def long_task(self):
+#     """Background task that runs a long function with progress reports."""
+#     verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
+#     adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+#     noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+#     message = ''
+#     total = random.randint(10, 50)
+#     for i in range(total):
+#         if not message or random.random() < 0.25:
+#             message = '{0} {1} {2}...'.format(random.choice(verb),
+#                                               random.choice(adjective),
+#                                               random.choice(noun))
+#         self.update_state(state='PROGRESS',
+#                           meta={'current': i, 'total': total,
+#                                 'status': message})
+#         time.sleep(1)
+#     return {'current': 100, 'total': 100, 'status': 'Task completed!',
+#             'result': 42}
 
 def is_same_address(request_dict, address, form_prefix='home'):
     is_same = True
